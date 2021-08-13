129c29cdfcb8935e4fed1fc284a17b412fa77fec
Marcus Kazmierczak <marcus@mkaz.com>
2021-08-13 08:22:34 -0700

    Add database insert and check

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Octoack
+
+A tool to query GitHub repos and send Slack notifications when new PRs are created by a specified list of users.
+
+
+
+## Setup
+
+1. Create Slack app
+
+2. Edit config octoack.toml add Slack webhook URL, users, repo
+
+3. Create sqlite database: octoack.sqlite
+
+```
+CREATE TABLE notices (url TEXT, timestamp INTEGER);
+```
+
+4. Use cron to run on your desired frequency
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use reqwest::blocking::ClientBuilder;
+use rusqlite::{params, Connection, Result};
 use serde::Deserialize;
-use std::collections::HashMap;
 use std::fs;
 use toml;
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize)]
 struct Config {
     Repos: Vec<String>,
     Users: Vec<User>,
 }
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize)]
 struct User {
     github: String,
     slack: String,
@@ -29,53 +29,54 @@ struct PullRequest {
     url: String,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     println!("Octoack!");
     let filename = "octoack.toml";
-    let apiurl = "https://api.github.com/search/issues";
-
-    let client = ClientBuilder::new().user_agent("Octoack/0.1.0").build()?;
 
     let config_file = fs::read_to_string(filename).unwrap();
     let config: Config = toml::from_str(&config_file).unwrap();
 
-    let mut query = HashMap::new();
-    query.insert("type".to_string(), "pr".to_string());
-    query.insert("is".to_string(), "open".to_string());
-    query.insert("draft".to_string(), "false".to_string());
+    let prs = get_pull_requests(config.clone());
+    let db = Connection::open("octoack.sqlite").unwrap();
 
-    for repo in config.Repos {
-        query.insert("repo".to_string(), repo);
+    // Send Slack Alerts
+    for pr in prs {
+        println!("Title: {}", pr.title);
 
-        // TODO: combine users into single query
-        for user in &config.Users {
-            query.insert("author".to_string(), user.github.clone());
-            let query_args = hash_to_query(query.clone());
-
-            let request_url = format!("{}?q={}", apiurl, query_args);
-            // println!("Request URL: {}", request_url);
-            //
-            let response = client.get(&request_url).send()?;
-            println!("Response: {:?}", response);
-            let results = response.json::<GHQueryResult>()?;
-            if results.total_count > 0 {
-                for pr in results.items {
-                    println!("Found: {} {}", pr.title, pr.url);
-                }
-            }
-            break;
+        let mut stmt = db.prepare("SELECT url FROM notices WHERE url = ?").unwrap();
+        let mut rows = stmt.query(params![pr.url]).unwrap();
+        if let Some(row) = rows.next().unwrap() {
+            println!("Already exists, ignore!");
+        } else {
+            println!("Does not exist, insert!");
+            db.execute(" INSERT INTO notices (url) VALUES (?)", params![pr.url])
+                .unwrap();
         }
     }
-
-    Ok(())
+    // check if exists in datbase
+    // - if not, send alert and add to database
 }
 
-fn hash_to_query(map: HashMap<String, String>) -> String {
-    let mut q = "".to_string();
+fn get_pull_requests(config: Config) -> Vec<PullRequest> {
+    let mut pulls: Vec<PullRequest> = Vec::new();
 
-    for (key, val) in &map {
-        q = format!("{}+{}:{}", q, key, val);
+    let apiurl = "https://api.github.com/search/issues";
+    let client = ClientBuilder::new()
+        .user_agent("Octoack/0.1.0")
+        .build()
+        .unwrap();
+
+    let mut query = "type:pr+is:open+draft:false".to_string();
+    for user in &config.Users {
+        query = format!("{}+author:{}", query, user.github.clone());
     }
 
-    return q.to_string();
+    for repo in config.Repos {
+        let request_url = format!("{}?q={}+repo:{}", apiurl, query, repo);
+        let response = client.get(&request_url).send().unwrap();
+        let results = response.json::<GHQueryResult>().unwrap();
+        pulls.extend(results.items);
+    }
+
+    return pulls;
 }
